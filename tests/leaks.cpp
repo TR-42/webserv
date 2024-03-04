38be1954b7fe0cb2e07751ed47a89da18f8859e8
kfujita <kfujita@student.42tokyo.jp>
2024-03-05 00:57:31 +0900

    テスト用のleaksを追加

---
+++ tests/leaks.cpp
@@ -0,0 +1,22 @@
+// - getpid
+// - system
+#include <unistd.h>
+
+// - getenv
+#include <stdlib.h>
+
+// - sprintf
+#include <stdio.h>
+
+#define DEBUG_LEAKS_CMD_LEN (64)
+
+extern "C" {
+
+__attribute__((destructor)) static void destructor(void)
+{
+	char cmd[DEBUG_LEAKS_CMD_LEN];
+
+	snprintf(cmd, DEBUG_LEAKS_CMD_LEN, "leaks -quiet %d > /dev/stderr", getpid());
+	system(cmd);
+}
+}
