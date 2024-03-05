96b06e05720ac9762b415b165bc45600b205ce0a
kfujita <kfujita@student.42tokyo.jp>
2024-03-05 23:42:49 +0900

    leakチェックを修正
    leaksはAddress Sanitizerと併用できないため、leaksは本番バイナリでのみ動作させるよう変更
    また、arm64なmacOS以外ではleak sanitizerを走らせるように設定

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,3 +4,30 @@ int main(void) {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+#ifdef DEBUG
+
+// - getpid
+// - system
+#include <unistd.h>
+
+// - getenv
+#include <stdlib.h>
+
+// - sprintf
+#include <stdio.h>
+
+#define DEBUG_LEAKS_CMD_LEN (64)
+
+extern "C" {
+
+__attribute__((destructor)) static void destructor(void)
+{
+	char cmd[DEBUG_LEAKS_CMD_LEN];
+
+	snprintf(cmd, DEBUG_LEAKS_CMD_LEN, "leaks -quiet %d > /dev/stderr", getpid());
+	system(cmd);
+}
+}
+
+#endif
