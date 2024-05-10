9a4cd669da1ce68b7738d9f613a5aae2999472de
kfujita <kfujita@student.42tokyo.jp>
2024-05-10 21:23:58 +0900

    CGIのテスト用プログラムを追加

---
+++ resources/php-cgi/inline.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+  </head>
+  <body>
+    <p><?php echo 'Hello, world!'; ?></p>
+    <p><?php
+		echo $_SERVER['QUERY_STRING']; ?></p>
+  </body>
+</html>
