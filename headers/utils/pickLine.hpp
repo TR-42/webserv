bac43684c27e8bcbf1e325e441e3078dfe4a20c1
kfujita <kfujita@student.42tokyo.jp>
2024-05-24 22:42:37 +0900

    cstdint使えない問題

--- headers/utils/pickLine.hpp
+++ headers/utils/pickLine.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cstdint>
 #include <types.hpp>
 #include <vector>
 
--- headers/utils/splitNameValue.hpp
+++ headers/utils/splitNameValue.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cstdint>
 #include <string>
 #include <types.hpp>
 #include <vector>
