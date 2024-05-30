2650b43f73360741af79263519676a2c10522a1d
tatashir <115047593+tatashir@users.noreply.github.com>
2024-05-30 21:23:27 +0900

    MappingNodeのテストを追加

--- tests/yaml/YamlParser.parse.tests.cpp
+++ tests/yaml/YamlParser.parse.tests.cpp
@@ -37,6 +37,43 @@ TEST(YamlParser, ScalarNode)
 	EXPECT_EQ(scalarNode->getValue(), "value");
 }
 
+TEST(YamlParser, MappingNode)
+{
+	std::vector<std::string> yamlLines = {
+		"key:",
+		"  key1: value1",
+		"  key2: value2",
+	};
+	MappingNode root("");
+	Logger logger;
+	EXPECT_TRUE(parse(yamlLines, root, logger));
+	EXPECT_EQ(root.getNodes().size(), 1);
+	if (root.getNodes().size() == 0)
+		return;
+	EXPECT_EQ(root.getNodes().at(0)->getKey(), "key");
+	const MappingNode *mappingNode = dynamic_cast<const MappingNode *>(root.getNodes().at(0));
+	EXPECT_NE(mappingNode, nullptr);
+	if (mappingNode == nullptr)
+		return;
+	EXPECT_EQ(mappingNode->getNodes().size(), 2);
+	if (mappingNode->getNodes().size() < 1)
+		return;
+	const ScalarNode *scalarNode1 = dynamic_cast<const ScalarNode *>(mappingNode->getNodes().at(0));
+	EXPECT_NE(scalarNode1, nullptr);
+	if (scalarNode1 != nullptr) {
+		EXPECT_EQ(scalarNode1->getKey(), "key1");
+		EXPECT_EQ(scalarNode1->getValue(), "value1");
+	}
+	if (mappingNode->getNodes().size() < 2)
+		return;
+	const ScalarNode *scalarNode2 = dynamic_cast<const ScalarNode *>(mappingNode->getNodes().at(1));
+	EXPECT_NE(scalarNode2, nullptr);
+	if (scalarNode2 != nullptr) {
+		EXPECT_EQ(scalarNode2->getKey(), "key2");
+		EXPECT_EQ(scalarNode2->getValue(), "value2");
+	}
+}
+
 }	 // namespace yaml
 
 }	 // namespace webserv
