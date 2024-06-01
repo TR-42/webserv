a5ca312fe0b067fa0ee3957cf142af7f34d170c5
kfujita <kfujita@student.42tokyo.jp>
2024-06-02 00:12:56 +0900

    PostFileServiceのバグ修正
    - 親ディレクトリが存在する場合には、その下にファイルを作成するように
    - 親ディレクトリが存在しない場合には、NotFoundを返すように
    - ReadOnly FSへの作成の場合はPermission Deniedを返すように
    - Indexファイルとして検知された場合は、上書きしないように

--- srcs/service/PostFileService.cpp
+++ srcs/service/PostFileService.cpp
@@ -18,6 +18,15 @@ namespace webserv
 
 #define BUFFER_SIZE 4096
 
+static std::string getDirName(const std::string &path)
+{
+	size_t pos = path.rfind('/');
+	if (pos == std::string::npos) {
+		return "";
+	}
+	return path.substr(0, pos + 1);
+}
+
 PostFileService::PostFileService(
 	const HttpRequest &request,
 	const RequestedFileInfo &requestedFileInfo,
@@ -29,21 +38,67 @@ PostFileService::PostFileService(
 {
 	std::string filePath(requestedFileInfo.getTargetFilePath());
 
-	// ファイルが存在しない場合も実行されるので注意
-	if (requestedFileInfo.getIsNotFound()) {
-		// TODO: 親ディレクトリが存在するか確認し、存在しなかったらNotFoundを返す
-	}
-
-	if (access(filePath.c_str(), W_OK) != 0) {
-		this->_response = this->_errorPageProvider.permissionDenied();
-		LS_INFO() << "Permission denied: " << filePath << std::endl;
+	// ディレクトリにファイルを作成することはできない
+	// AutoIndexファイルへの書き込みは許可しない
+	// PathInfoがあるということは、目的でないファイルである可能性があるため、許可しない
+	if (requestedFileInfo.getIsDirectory() || requestedFileInfo.getIsAutoIndexFile() || !requestedFileInfo.getCgiPathInfo().empty()) {
+		this->_response = this->_errorPageProvider.methodNotAllowed();
+		LS_INFO() << "Method not allowed: " << filePath << std::endl;
 		return;
 	}
 
-	this->_fd = open(filePath.c_str(), O_WRONLY | O_TRUNC);
+	// ファイルが存在しなかった場合は、親ディレクトリが存在する場合にのみファイルの新規作成を行う
+	if (requestedFileInfo.getIsNotFound()) {
+		std::string dirPath = getDirName(filePath);
+		struct stat statBuf;
+		if (stat(dirPath.c_str(), &statBuf) != 0) {
+			errno_t err = errno;
+			this->_response = this->_errorPageProvider.notFound();
+			LS_INFO()
+				<< "Parent Directory Not Found: " << requestedFileInfo.getDocumentRoot()
+				<< " (err: " << std::strerror(err) << ")"
+				<< std::endl;
+			return;
+		}
+		LS_LOG()
+			<< "Directory info: "
+			<< "Path: " << dirPath << ", "
+			<< "User ID: " << statBuf.st_uid << ", "
+			<< "Group ID: " << statBuf.st_gid << ", "
+			<< "File size: " << statBuf.st_size << ", "
+			<< "Block size: " << statBuf.st_blksize << ", "
+			<< "Block count: " << statBuf.st_blocks << ", "
+			<< "Permissions: " << utils::modeToString(statBuf.st_mode) << ", "
+			<< "Last modified: " << utils::getHttpTimeStr(statBuf.st_mtime)
+			<< std::endl;
+		if (access(dirPath.c_str(), W_OK) != 0) {
+			this->_response = this->_errorPageProvider.permissionDenied();
+			LS_INFO() << "Permission denied: " << filePath << std::endl;
+			return;
+		}
+
+		this->_fd = open(
+			filePath.c_str(),
+			O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+		);
+	} else {
+		if (access(filePath.c_str(), W_OK) != 0) {
+			this->_response = this->_errorPageProvider.permissionDenied();
+			LS_INFO() << "Permission denied: " << filePath << std::endl;
+			return;
+		}
+
+		this->_fd = open(filePath.c_str(), O_WRONLY | O_TRUNC);
+	}
+
 	if (this->_fd < 0) {
 		errno_t err = errno;
-		this->_response = this->_errorPageProvider.internalServerError();
+		if (err == EACCES) {
+			this->_response = this->_errorPageProvider.permissionDenied();
+		} else {
+			this->_response = this->_errorPageProvider.internalServerError();
+		}
 		LS_ERROR()
 			<< "Failed to open file: " << filePath
 			<< " (err: " << std::strerror(err) << ")"
@@ -89,6 +144,10 @@ ServiceEventResultType PostFileService::onEventGot(
 
 	size_t dataSize = this->_request.getBody().size() - this->_writtenSize;
 	size_t writeSize = std::min(dataSize, static_cast<size_t>(INT_MAX));
+	CS_LOG()
+		<< "Writing " << writeSize << " / " << this->_request.getBody().size() << " bytes"
+		<< " (already written: " << this->_writtenSize << ")"
+		<< std::endl;
 	ssize_t writtenLength = write(
 		this->_fd,
 		this->_request.getBody().data() + this->_writtenSize,
@@ -106,7 +165,7 @@ ServiceEventResultType PostFileService::onEventGot(
 
 	this->_writtenSize += writtenLength;
 	if (this->_request.getBody().size() == this->_writtenSize) {
-		LS_DEBUG() << "Write " << writtenLength << " bytes" << std::endl;
+		LS_DEBUG() << "Write " << writtenLength << " bytes Completed" << std::endl;
 		return ServiceEventResult::COMPLETE;
 	}
 
