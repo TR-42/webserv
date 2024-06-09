937380669d3add39cd44fd58068c5b39ae043c51
kfujita <kfujita@student.42tokyo.jp>
2024-06-09 16:37:25 +0900

    chunked bodyのテストを追加

--- test-ci/http-test.py
+++ test-ci/http-test.py
@@ -143,8 +143,8 @@ def TestPathInfo():
 
 	conn.close()
 
-def TestBody():
-	BODY_STR_LIST = ["Hello, World!", "from Python"] * 5000
+def TestBody(count = 100):
+	BODY_STR_LIST = ["Hello, World!", "from Python"] * count
 	BODY_STR = "\n".join(BODY_STR_LIST)
 	conn = http.client.HTTPConnection(host)
 	conn.request(
@@ -200,6 +200,66 @@ def TestBody():
 
 	conn.close()
 
+def TestChunkedBody(count = 100):
+	BODY_STR_LIST = ["Hello, World!", "from Python"] * count
+	BODY_STR = "\n".join(BODY_STR_LIST)
+	conn = http.client.HTTPConnection(host)
+	conn.request(
+		"POST",
+		"/resources/sh-cgi/document.sh/abc/def?query=value&key=value2&chunked=true",
+		headers={
+			"Host": host,
+			"Accept-Encoding": "identity",
+			"Content-Type": "text/plain",
+			"Transfer-Encoding": "chunked",
+		},
+		body=BODY_STR,
+		encode_chunked=True,
+	)
+	response = conn.getresponse()
+
+	cmp("status", response.status, 200)
+	cmp("reason", response.reason, "OK")
+	headers = dict(response.getheaders())
+	cmp("header count", len(headers), 4)
+	cmp("content-type", headers["Content-Type"], "text/plain")
+	cmp("content-length", headers["Content-Length"], lambda actual: actual.isdigit() and int(actual) > 0)
+	cmp("server", headers["Date"] != "", True)
+
+	actualBody = response.read().decode()
+
+	expected = [
+		"Content-Type: text/plain",
+		f"Content-Length: {len(BODY_STR)}",
+		"Gateway Interface: CGI/1.1",
+		"Path Info: /abc/def",
+		f"Path Translated: {PROJ_ROOT_DIR}/resources/sh-cgi/document.sh",
+		"Query String: query=value&key=value2&chunked=true",
+		"Request Method: POST",
+		"Script Name: /resources/sh-cgi/document.sh",
+		"Remote Address: 127.0.0.1",
+		lambda actual: actual.startswith("Remote Port: ") and actual[14:].isdigit() and int(actual[14:]) > 0 and int(actual[14:]) < 65536,
+		f"Server Name: {host}",
+		"Server Port: 80",
+		"Server Protocol: HTTP/1.1",
+		"Server Software: webserv/1.0",
+		"Hello, World!",
+		"",
+		"HTTP_ACCEPT_ENCODING=identity",
+		f"HTTP_HOST={host}",
+		"HTTP_TRANSFER_ENCODING=chunked",
+		"",
+	]
+	expected.extend(BODY_STR_LIST)
+
+	cmp_list(
+		"body",
+		actualBody.split("\n"),
+		expected
+	)
+
+	conn.close()
+
 
 print("Running tests...")
 print()
@@ -214,6 +274,10 @@ TestPathInfo()
 print()
 print("TestBody")
 TestBody()
+
+print()
+print("TestChunkedBody")
+TestChunkedBody()
 
 print()
 
