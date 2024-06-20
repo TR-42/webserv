59c95e7c9560f64060043c8fec86e102278d35cb
kfujita <kfujita@student.42tokyo.jp>
2024-06-21 08:40:35 +0900

    ClientSocketのエラーでサーバー全体を終了しないようにした

--- srcs/socket/ClientSocket.cpp
+++ srcs/socket/ClientSocket.cpp
@@ -96,10 +96,10 @@ PollEventResultType ClientSocket::_processPollIn(
 		try {
 			this->_readBuf = new uint8_t[RECV_BUFFER_SIZE];
 		} catch (const std::exception &e) {
-			CS_FATAL()
+			CS_WARN()
 				<< "Failed to allocate memory for read buffer: " << e.what()
 				<< std::endl;
-			return PollEventResult::ERROR;
+			return PollEventResult::OK;
 		}
 	}
 
@@ -112,10 +112,10 @@ PollEventResultType ClientSocket::_processPollIn(
 
 	if (recvSize < 0) {
 		const char *errorStr = std::strerror(errno);
-		CS_FATAL()
+		CS_ERROR()
 			<< "recv() failed: " << errorStr
 			<< std::endl;
-		return PollEventResult::ERROR;
+		return PollEventResult::DISPOSE_REQUEST;
 	}
 
 	if (recvSize == 0) {
@@ -308,10 +308,10 @@ PollEventResultType ClientSocket::_processPollOut()
 
 	if (sendSize < 0) {
 		const char *errorStr = std::strerror(errno);
-		CS_FATAL()
+		CS_ERROR()
 			<< "send() failed: " << errorStr
 			<< std::endl;
-		return PollEventResult::ERROR;
+		return PollEventResult::DISPOSE_REQUEST;
 	}
 
 	if (sendSize == 0) {
